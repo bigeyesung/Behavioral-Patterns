effc36e6a26ddb9e0dad80e633ca22b8a668ca3f
contiliosung <sungchenhsi@gmail.com>
2019-11-02 18:07:09 +0000

    -Command.py: func add, minor change

--- Command.py
+++ Command.py
@@ -10,3 +10,7 @@ class SimpleCommand(Command):
 
     def __init__(self, payload: str) -> None:
         self._payload = payload
+
+    def execute(self) -> None:
+        print(f"SimpleCommand: See, I can do simple things like printing"
+              f"({self._payload})")
