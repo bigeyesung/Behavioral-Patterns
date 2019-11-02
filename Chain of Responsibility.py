e627feee686aa77ec40e212f8fa86e0418a655fe
contiliosung <sungchenhsi@gmail.com>
2019-11-02 17:48:14 +0000

    -Chain of Responsibility: func add, minor change

--- Chain of Responsibility.py
+++ Chain of Responsibility.py
@@ -2,3 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any, Optional
 
 class Handler(ABC):
+
+    @abstractmethod
+    def set_next(self, handler: Handler) -> Handler:
+        pass
