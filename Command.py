bec913b63ff6a77f75cf42bd810a9457f3e16f34
contiliosung <sungchenhsi@gmail.com>
2019-11-02 18:12:06 +0000

    -Command.py: func add, minor change

--- Command.py
+++ Command.py
@@ -60,3 +60,7 @@ if __name__ == "__main__":
     invoker = Invoker()
     invoker.set_on_start(SimpleCommand("Say Hi!"))
     receiver = Receiver()
+    invoker.set_on_finish(ComplexCommand(
+    receiver, "Send email", "Save report"))
+
+    invoker.do_something_important()
