24cc11cd5e407df676902e5918a4867afe7d0300
contiliosung <sungchenhsi@gmail.com>
2019-11-02 17:48:37 +0000

    -Chain of Responsibility: func add, minor change

--- Chain of Responsibility.py
+++ Chain of Responsibility.py
@@ -6,3 +6,7 @@ class Handler(ABC):
     @abstractmethod
     def set_next(self, handler: Handler) -> Handler:
         pass
+
+    @abstractmethod
+    def handle(self, request) -> Optional[str]:
+        pass
